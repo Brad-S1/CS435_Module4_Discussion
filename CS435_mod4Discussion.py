33037f12f18c5e9a5663a039b3a7a39fc2aa7d22
Brad <114127564+Brad-S1@users.noreply.github.com>
2025-10-14 22:24:23 -0700

    create files for discussion

---
+++ CS435_mod4Discussion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 14 22:21:16 2025
+
+@author: bradsommer
+
+This code is adapted from CS 435 Module 4 Exercise 1 & 2 sample code written by
+Dr. G.
+"""
